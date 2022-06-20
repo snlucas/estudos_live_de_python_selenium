5fdfa67cc80eaa1534e1067d158f392fcf8acf4e
Lucas S Nogueira <lucas.lambda.101@gmail.com>
2022-06-20 01:19:51 +0000

    Add explicacoes das funcoes print e input

---
+++ curso_rapido/explicacoes/print_input.py
@@ -0,0 +1,11 @@
+# print <- Serve para escreve na tela <-- É uma função
+
+# print('Oi, meu nome é Lucas\n')
+# print("Oi, meu nome é Fausto")
+
+
+# input <- Escreve e espera uma entrada <-- É uma função <-- pode ter uma variável armazenando seu valor
+
+idade = input('Qual a sua idade? ')
+
+print(idade)
